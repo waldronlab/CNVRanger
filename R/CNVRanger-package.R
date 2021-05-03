7badab5655d26ec436a7833b2f6099f7fee56bbd
lgeistlinger <ludwig.geistlinger@gmail.com>
2021-05-02 21:09:09 -0400

    fix ExperimentHub version

--- R/CNVRanger-package.R
+++ R/CNVRanger-package.R
@@ -2,6 +2,6 @@
 #' @importFrom IRanges extractList subsetByOverlaps
 #' @importFrom grDevices dev.off pdf extendrange
 #' @importFrom methods as is
-#' @importFrom stats ecdf median pchisq qbeta qchisq
+#' @importFrom stats ecdf lm median pchisq qbeta qchisq
 #' @importFrom utils download.file read.table unzip write.table
 NULL
